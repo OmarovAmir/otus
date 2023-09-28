1ed68b4ac85d88b481074fe8f3fba19680564f1f
OmarovAmir <omarov.amir.95@gmail.com>
2023-09-28 23:06:23 +0700

    Реализация pool_allocator через две unordered_map

--- main.cpp
+++ main.cpp
@@ -1,18 +1,65 @@
 #include "pool_allocator.hpp"
 #include <chrono>
 #include <iostream>
+#include <map>
+#include <random>
 #include <vector>
 
 int main()
 {
-    auto begin = std::chrono::steady_clock::now();
-    // std::vector<int, pool_allocator<int>> v;
-    std::vector<int> v;
+    std::random_device rd;
+    std::mt19937::result_type seed = rd() ^ ((std::mt19937::result_type)
+                                                 std::chrono::duration_cast<std::chrono::seconds>(
+                                                     std::chrono::system_clock::now().time_since_epoch())
+                                                     .count() +
+                                             (std::mt19937::result_type)
+                                                 std::chrono::duration_cast<std::chrono::microseconds>(
+                                                     std::chrono::high_resolution_clock::now().time_since_epoch())
+                                                     .count());
 
-    for (int i = 0; i < 10000000; ++i)
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<std::size_t> distrib(2048, 4096);
+
+    auto begin = std::chrono::steady_clock::now();
+
+    for (unsigned long j = 0; j < 1000; ++j)
     {
-        v.push_back(i);
+        auto n = distrib(gen);
+
+        std::vector<int, pool_allocator<int>> v[10];
+        // std::vector<int> v[10];
+        for (int i = 0; i < 10; ++i)
+        {
+            for (std::size_t j = 0; j < n; ++j)
+            {
+                v[i].push_back(j);
+            }
+            v[i].clear();
+        }
+
+        // std::list<int, pool_allocator<int>> l[10];
+        // std::list<int> l[10];
+        // for (int i = 0; i < 10; ++i)
+        // {
+        //     for (std::size_t j = 0; j < n; ++j)
+        //     {
+        //         l[i].push_back(j);
+        //     }
+        //     l[i].clear();
+        // }
+
+        // std::map<int,int, std::less<int>, pool_allocator<std::pair<int,int>>> m[10];
+        // std::map<int,int> m[10];
+        // for (int i = 0; i < 10; ++i)
+        // {
+        //     for (std::size_t j = 0; j < n; ++j)
+        //     {
+        //         m[i][j] = j + 1;
+        //     }
+        //     m[i].clear();
+        // }
     }
+
     auto end = std::chrono::steady_clock::now();
 
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
--- pool_allocator.hpp
+++ pool_allocator.hpp
@@ -4,50 +4,19 @@
 #include <memory>
 #include <unordered_map>
 
-#define SHOW_FUNC (std::cout << __PRETTY_FUNCTION__ << std::endl)
+#define LOG_NAME_ON (false)
+#define LOG_ON (false)
 
-template <typename T>
-class pool_allocator
-{
-    using pool_item = std::shared_ptr<T>;
-    using pool = std::list<pool_item>;
-    using pool_map = std::unordered_multimap<std::size_t, pool>;
-
-    pool_map free_pool;
-    pool_map used_pool;
-
-    void show_state(const pool_allocator<T> &allocator)
-    {
-        auto show_pool = [](const auto &pool_map)
-        {
-            std::cout << "size: " << pool_map.size() << std::endl;
-            for (const auto &pool : pool_map)
-            {
-                std::cout << "[" << pool.first << "].size: " << pool.second.size() << std::endl;
-                if (!pool.second.empty())
-                {
-                    std::cout << "[" << pool.first << "] = { ";
-                    auto pool_item = pool.second.cbegin();
-                    while (pool_item != pool.second.cend())
-                    {
-                        if (pool_item != pool.second.cbegin())
-                        {
-                            std::cout << ", ";
-                        }
-                        std::cout << pool_item->get();
-                        ++pool_item;
-                    }
-                    std::cout << " };" << std::endl;
-                }
-            }
-        };
-        std::cout << "----- free_pool -----" << std::endl;
-        show_pool(allocator.free_pool);
-        std::cout << "----- used_pool -----" << std::endl;
-        show_pool(allocator.used_pool);
+#define SHOW_FUNC                                      \
+    if (LOG_NAME_ON)                                   \
+    {                                                  \
+        std::cout << __PRETTY_FUNCTION__ << std::endl; \
     }
 
-  public:
+template <typename T>
+struct pool_allocator
+{
+
     using value_type = T;
 
     using propagate_on_container_copy_assignment = std::true_type;
@@ -57,130 +26,123 @@ class pool_allocator
     template <class U>
     struct rebind
     {
-        using other = pool_allocator<U>;
+        typedef pool_allocator<U> other;
     };
 
-    pool_allocator() noexcept
+    using pool_item_t = std::shared_ptr<T>;
+    using pool_item_weak_t = std::weak_ptr<T>;
+    using pool_t = std::list<pool_item_t>;
+
+    using alloc_map_t = std::unordered_map<std::size_t, pool_t>;
+    using dealloc_map_t = std::unordered_map<T *, pool_item_weak_t>;
+
+    static alloc_map_t alloc_map;
+    static dealloc_map_t dealloc_map;
+
+    void show_state(const pool_allocator<T> &allocator)
     {
-        free_pool.emplace(1, pool{});
-        used_pool.emplace(1, pool{});
-        // SHOW_FUNC;
-        // show_state(*this);
-    };
-    pool_allocator([[maybe_unused]] const pool_allocator<T> &allocator) noexcept
-    {
-        free_pool = allocator.free_pool;
-        used_pool = allocator.used_pool;
-        // SHOW_FUNC;
-        // show_state(*this);
-        // show_state(allocator);
-    };
-    ~pool_allocator() noexcept
-    {
-        /*
-             1. memoryClean для всех pool_item в free_pool и used_pool
-             2. release для всех pool_item
-        */
-        auto remove_pool = [](auto &pool_map)
+        if (!LOG_ON)
         {
-            for (auto &pool : pool_map)
+            return;
+        }
+        if (!allocator.alloc_map.empty())
+        {
+            std::cout << "-----  alloc_map  -----" << std::endl;
+            for (const auto &pool : allocator.alloc_map)
             {
                 if (!pool.second.empty())
                 {
-                    for (auto pool_item : pool.second)
+                    std::cout << "[" << pool.first << "][" << pool.second.size() << "]" << std::endl;
+                    for (const auto &pool_item : pool.second)
                     {
-                        pool_item.reset();
+                        std::cout << "{ " << pool_item.get() << " }" << std::endl;
                     }
-                    pool.second.clear();
                 }
             }
-        };
-        remove_pool(free_pool);
-        remove_pool(used_pool);
-        // SHOW_FUNC;
-        // show_state(*this);
+            std::cout << std::endl;
+        }
+        if (!allocator.dealloc_map.empty())
+        {
+            std::cout << "----- dealloc_map -----" << std::endl;
+            for (const auto &pool : allocator.dealloc_map)
+            {
+                std::cout << "{ " << pool.first << " }" << std::endl;
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    pool_allocator() = default;
+    pool_allocator(const pool_allocator<T> &allocator) = default;
+    pool_allocator(pool_allocator<T> &&allocator) = default;
+    pool_allocator &operator=(const pool_allocator<T> &allocator) = default;
+    pool_allocator &operator=(pool_allocator<T> &&allocator) = default;
+
+    ~pool_allocator() noexcept
+    {
+        SHOW_FUNC;
+        dealloc_map.clear();
+        alloc_map.clear();
+        show_state(*this);
     }
 
     T *allocate([[maybe_unused]] std::size_t n)
     {
-        /*
-             Поиск pool размера n в free_pool
-             Если pool не нашёлся
-                  Создаем pool
-                  Добавляем его в free_pool[n]
-             Если в pool нет свободного pool_item
-                  Выделяем память static_cast<T*>(::operator new(n*sizeof(T)))
-                  Если не выделилась
-                       std::bad_alloc();
-                  Создаём новый pool_item
-             Переносим pool_item в used_pool[n]->second
-             Возвращаем указатель на данные в pool_item через get()
-        */
-
-        pool_item result;
-        auto find_free_pool = free_pool.find(n);
-        auto find_used_pool = used_pool.find(n);
-        if (find_free_pool != free_pool.end())
+        SHOW_FUNC;
+        auto find_list = alloc_map.find(n);
+        if (find_list == alloc_map.end())
         {
-            if (!find_free_pool->second.empty())
+            pool_item_t new_pool_item{static_cast<T *>(::operator new(sizeof(T) * n))};
+            if (!new_pool_item)
             {
-                result = std::move(find_free_pool->second.back());
-                find_used_pool->second.push_back(result);
+                std::bad_alloc();
             }
-            else
+            pool_t new_pool{new_pool_item};
+            alloc_map.emplace(n, std::move(new_pool));
+        }
+
+        find_list = alloc_map.find(n);
+        if (dealloc_map.find(find_list->second.front().get()) != dealloc_map.end())
+        {
+            pool_item_t new_pool_item{static_cast<T *>(::operator new(sizeof(T) * n))};
+            if (!new_pool_item)
             {
-                pool_item new_item = pool_item(static_cast<T *>(::operator new[](sizeof(T) * n)));
-                result = new_item;
-                find_used_pool->second.push_back(result);
+                std::bad_alloc();
             }
+            find_list->second.emplace_back(std::move(new_pool_item));
+            dealloc_map.emplace(find_list->second.back().get(), find_list->second.back());
+            show_state(*this);
+            return find_list->second.back().get();
         }
         else
         {
-            pool_item new_item = pool_item(static_cast<T *>(::operator new[](sizeof(T) * n)));
-            free_pool.emplace(n, pool{});
-            result = new_item;
-            used_pool.emplace(n, pool{std::move(new_item)});
+            find_list->second.emplace_back(find_list->second.front());
+            find_list->second.pop_front();
+            dealloc_map.emplace(find_list->second.back().get(), find_list->second.back());
+            show_state(*this);
+            return find_list->second.back().get();
         }
-        // SHOW_FUNC;
-        // std::cout << n << std::endl;
-        // show_state(*this);
-        return result.get();
     }
 
-    void deallocate([[maybe_unused]] T *p, [[maybe_unused]] std::size_t n)
+    void deallocate(T *p, [[maybe_unused]] std::size_t n)
     {
-        pool_item remove_item;
-        auto find_free_pool = free_pool.find(n);
-        auto find_used_pool = used_pool.find(n);
-        if (find_used_pool != used_pool.end())
+        SHOW_FUNC;
+        auto item = dealloc_map.find(p);
+        if (item != dealloc_map.end())
         {
-            if (!find_used_pool->second.empty())
-            {
-                for (auto pool_item : find_used_pool->second)
-                {
-                    if (pool_item.get() == p)
-                    {
-                        remove_item = pool_item;
-                        find_used_pool->second.remove(pool_item);
-                        break;
-                    }
-                }
-                find_free_pool->second.push_back(std::move(remove_item));
-            }
-            else
-            {
-                ::operator delete[](p, sizeof(T) * n);
-            }
+            dealloc_map.erase(item);
+            show_state(*this);
+            return;
         }
-        else
-        {
-            ::operator delete[](p, sizeof(T) * n);
-        }
-        // SHOW_FUNC;
-        // std::cout << n << std::endl;
-        // show_state(*this);
+        std::bad_alloc();
     }
 };
+
+template <typename T>
+typename pool_allocator<T>::alloc_map_t pool_allocator<T>::alloc_map;
+
+template <typename T>
+typename pool_allocator<T>::dealloc_map_t pool_allocator<T>::dealloc_map;
 
 template <class T, class U>
 constexpr bool operator==([[maybe_unused]] const pool_allocator<T> &a1, [[maybe_unused]] const pool_allocator<U> &a2) noexcept

e2e64443ea74c3779b3d3cd67b3e1ffc67f1bd4d
OmarovAmir <omarov.amir.95@gmail.com>
2023-10-02 00:15:53 +0700

    Доработал для std::vector/list/tuple

--- print_ip.hpp
+++ print_ip.hpp
@@ -23,7 +23,7 @@ struct type_select<T, typename std::enable_if<std::is_integral<T>::value>::type>
     {
         const uint8_t* byte_ptr = reinterpret_cast<const uint8_t*>(&v);
         auto size = sizeof(v);
-        for (unsigned int i = 1; i <= sizeof(v); ++i)
+        for (unsigned int i = 1; i <= size; ++i)
         {
             if (i != 1)
             {
@@ -41,6 +41,78 @@ struct type_select<T, typename std::enable_if<std::is_same<std::string, T>::valu
     static void print_ip(const T& v)
     {
         std::cout << v << std::endl;
+    }
+};
+
+template <typename T>
+struct type_select<std::vector<T>>
+{
+    static void print_ip(const std::vector<T>& v)
+    {
+        for (auto i = v.cbegin(); i != v.cend(); ++i)
+        {
+            if (i != v.cbegin())
+            {
+                std::cout << ".";
+            }
+            std::cout << *i;
+        }
+        std::cout << std::endl;
+    }
+};
+
+template <typename T>
+struct type_select<std::list<T>>
+{
+    static void print_ip(const std::list<T>& v)
+    {
+        for (auto i = v.cbegin(); i != v.cend(); ++i)
+        {
+            if (i != v.cbegin())
+            {
+                std::cout << ".";
+            }
+            std::cout << *i;
+        }
+        std::cout << std::endl;
+    }
+};
+
+template <typename First, typename... T>
+struct all_same_type
+{
+    constexpr static bool value = std::is_same_v<std::tuple<First, T...>, std::tuple<T..., First>>;
+};
+
+template <typename... T>
+struct all_same_type<std::tuple<T...>> : all_same_type<T...>
+{
+};
+
+template <typename... T>
+constexpr bool all_same_type_v = all_same_type<T...>::value;
+
+template <typename... T>
+struct type_select<std::tuple<T...>>
+{
+    template <typename TupleT, std::size_t... Is>
+    static void print_tuple(std::ostream& os, const TupleT& tp, std::index_sequence<Is...>)
+    {
+        auto printElem = [&os](const auto& x, size_t id)
+        {
+            if (id > 0)
+                os << ".";
+            os << x;
+        };
+
+        (printElem(std::get<Is>(tp), Is), ...);
+        os << std::endl;
+    }
+
+    static void print_ip(const std::tuple<T...>& v)
+    {
+        static_assert(all_same_type_v<std::tuple<T...>>);
+        print_tuple(std::cout, v, std::make_index_sequence<std::tuple_size<std::tuple<T...>>::value>{});
     }
 };
 

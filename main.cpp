9cbe2f82b137423fd157c6ff748797b8ea16c4bd
OmarovAmir <omarov.amir.95@gmail.com>
2023-10-01 12:30:49 +0700

    Подготовил ветку для работы над ДЗ

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "pool_allocator.hpp"
+#include "print_ip.hpp"
 #include <chrono>
 #include <functional>
 #include <iostream>
@@ -10,51 +10,5 @@
 
 int main()
 {
-    std::random_device rd;
-    std::mt19937::result_type seed = rd() ^ ((std::mt19937::result_type)
-                                                 std::chrono::duration_cast<std::chrono::seconds>(
-                                                     std::chrono::system_clock::now().time_since_epoch())
-                                                     .count() +
-                                             (std::mt19937::result_type)
-                                                 std::chrono::duration_cast<std::chrono::microseconds>(
-                                                     std::chrono::high_resolution_clock::now().time_since_epoch())
-                                                     .count());
-
-    std::mt19937 gen(seed);
-    std::uniform_int_distribution<std::size_t> distrib(5, 5);
-
-    auto n = distrib(gen);
-
-    auto begin = std::chrono::steady_clock::now();
-
-
-    std::vector<int, pool_allocator<int>> v;
-    std::list<int, pool_allocator<int>> l;
-    std::map<int, int, std::less<int>, pool_allocator<std::pair<int, int>>> m;
-    // std::unordered_map<int, int, std::hash<int>, std::equal_to<int>, pool_allocator<std::pair<int, int>>> um;// ???
-    
-    // std::vector<int> v;
-    // std::list<int> l;
-    // std::map<int,int> m;
-    // std::unordered_map<int,int> um;
-
-    for (std::size_t i = 0; i < n; ++i)
-    {
-        v.push_back(i);
-        l.push_back(i);
-        m[i] = i + 1;
-        // um[i] = i + 1;
-    }
-
-    v.clear();
-    l.clear();
-    m.clear();
-    // um.clear();
-
-    auto end = std::chrono::steady_clock::now();
-
-    auto elapsed_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    std::cout << "The time: " << elapsed_ms.count() << " ms\n";
-
     return 0;
 }
---
+++ test_print_ip.cpp
@@ -0,0 +1,7 @@
+#include "print_ip.hpp"
+#include <gtest/gtest.h>
+
+TEST(print_ip_tests, test1)
+{
+    EXPECT_TRUE(true);
+}

39398a738a9a8a9b8d3186b56e43034b39063e27
omarov.amir <Amir.Omarov@infotecs.ru>
2023-11-07 14:14:15 +0700

    Добавил конструкторы копирования и перемещения, а также копирующий и перемещающий операторы присваивания

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ struct matrix
     std::shared_ptr<std::map<std::array<std::size_t, dimension>, T>> _data;
     T _default_value;
     std::size_t _level;
-    const std::size_t _last_level = 1;
+    static constexpr std::size_t _last_level = 1;
 
     explicit matrix()
         : _index{std::make_shared<std::array<std::size_t, dimension>>()}
@@ -20,18 +20,7 @@ struct matrix
         , _default_value{default_value}
         , _level{dimension}
     {
-    }
-
-    explicit matrix(
-        const std::array<std::size_t, dimension>& index,
-        const std::map<std::array<std::size_t, dimension>, T>& data,
-        T _default_value = default_value,
-        std::size_t level = dimension)
-        : matrix(std::make_shared<std::array<std::size_t, dimension>>(index),
-                 std::make_shared<std::map<std::array<std::size_t, dimension>, T>>(data),
-                 _default_value, 
-                 level)
-    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
     }
 
     explicit matrix(
@@ -44,6 +33,88 @@ struct matrix
         , _default_value{_default_value}
         , _level{level}
     {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    explicit matrix(
+        std::shared_ptr<std::array<std::size_t, dimension>>&& index,
+        std::shared_ptr<std::map<std::array<std::size_t, dimension>, T>>&& data,
+        T _default_value = default_value,
+        std::size_t level = dimension)
+        : _index{std::move(index)}
+        , _data{std::move(data)}
+        , _default_value{_default_value}
+        , _level{level}
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    explicit matrix(
+        const std::array<std::size_t, dimension>& index,
+        const std::map<std::array<std::size_t, dimension>, T>& data,
+        T _default_value = default_value,
+        std::size_t level = dimension)
+        : matrix(std::make_shared<std::array<std::size_t, dimension>>(index),
+                 std::make_shared<std::map<std::array<std::size_t, dimension>, T>>(data),
+                 _default_value, 
+                 level)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    explicit matrix(
+        std::array<std::size_t, dimension>&& index,
+        std::map<std::array<std::size_t, dimension>, T>&& data,
+        T _default_value = default_value,
+        std::size_t level = dimension)
+        : matrix(std::make_shared<std::array<std::size_t, dimension>>(std::move(index)),
+                 std::make_shared<std::map<std::array<std::size_t, dimension>, T>>(std::move(data)),
+                 _default_value, 
+                 level)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    matrix(
+        const matrix<T, default_value, dimension>& other)
+        : matrix(*(other._index),
+                 *(other._data),
+                 other._default_value, 
+                 other._level)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    matrix(
+        matrix<T, default_value, dimension>&& other)
+        : matrix(std::move(*(other._index)),
+                 std::move(*(other._data)),
+                 other._default_value, 
+                 other._level)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    matrix<T, default_value, dimension>& operator=(
+        const matrix<T, default_value, dimension>& other)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        this->_index = {*(other._index)};
+        this->_data = {*(other._data)};
+        this->_default_value = other._default_value;
+        this->_level = _level;
+        return *this;
+    }
+
+    matrix<T, default_value, dimension>& operator=(
+        matrix<T, default_value, dimension>&& other)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        *(this->_index) = std::move(*(other._index));
+        *(this->_data) = std::move(*(other._data));
+        this->_default_value = other._default_value;
+        this->_level = _level;
+        return *this;
     }
 
     auto& operator[](std::size_t n)
@@ -62,7 +133,7 @@ struct matrix
         return *this;
     }
 
-    T& get()
+    T get()
     {
         if (_data->find((*_index)) != _data->end())
         {
@@ -95,31 +166,21 @@ struct matrix
         }
         return (res == value);
     }
-    
-    auto clone()
-    {
-        matrix<T, default_value, dimension> clone{*_index, *_data, _default_value, _level};
-        return clone;
-    }
 };
 
 int main()
 {
     {
-        matrix<int, -1> matrix;
-        assert(matrix.size() == 0); // все ячейки свободны
-        auto a = matrix[0][0];
-        assert(a == -1);
-        assert(matrix.size() == 0);
-        matrix[100][100] = 314;
-        assert(matrix[100][100] == 314);
-        assert(matrix.size() == 1);
-        auto x = matrix.clone();
-        assert(x.size() == 1);
-        matrix[0][0] = 3;
-        assert(matrix.size() == 2);
-        assert(x.size() == 1);
+        // matrix<int, -1> matrix;
+        // assert(matrix.size() == 0); // все ячейки свободны
 
+        // auto a = matrix[0][0];
+        // assert(a == -1);
+        // assert(matrix.size() == 0);
+
+        // matrix[100][100] = 314;
+        // assert(matrix[100][100] == 314);
+        // assert(matrix.size() == 1);      
 
         // // выведется одна строка
         // // 100100314
@@ -132,6 +193,32 @@ int main()
         //     std::cout << x << y << v << std::endl;
         // }
     }
+
+    {
+        matrix<int, -1> matrix;
+        assert(matrix.size() == 0);
+        matrix[0][0] = 10;
+        matrix[0][1] = 20;
+        assert(matrix.size() == 2);
+        std::cout << std::endl << std::endl;
+        auto x = matrix;
+        assert(x.size() == 2);
+        assert(matrix.size() == 2);
+        std::cout << std::endl << std::endl;
+        auto b = std::move(matrix);
+        assert(b.size() == 2);
+        assert(matrix.size() == 0);
+        std::cout << std::endl << std::endl;
+        auto k{b};
+        assert(b.size() == 2);
+        assert(k.size() == 2);
+        std::cout << std::endl << std::endl;
+        auto l{std::move(b)};
+        assert(b.size() == 0);
+        assert(k.size() == 2);
+
+    }
+
 
     return 0;
 }

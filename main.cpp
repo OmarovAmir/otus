0e5c171750fa59a2c6c560d9c546306c29eeae46
OmarovAmir <omarov.amir.95@gmail.com>
2023-11-26 13:35:11 +0700

    Добавил рекурсивный проход по файловой системе до необходимой глубины

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <options.hpp>
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
 
 int main(int argc, char** argv)
 {
@@ -12,6 +14,33 @@ int main(int argc, char** argv)
             opts.print(std::cout);
             return 0;
         }
+
+        fs::path path = vm["include-paths"].as<paths>()[0];
+        std::size_t depth = vm["depth"].as<std::size_t>();
+
+        if (fs::exists(path))
+        {
+            if (fs::is_regular_file(path))
+                std::cout << path << " size is " << fs::file_size(path) << '\n';
+
+            else if (fs::is_directory(path))
+            {
+                std::cout << path << " is a directory containing:\n";
+
+                for (auto ri = fs::recursive_directory_iterator(path); ri != fs::recursive_directory_iterator(); ++ri)
+                {
+                    if(ri.depth() > static_cast<int>(depth))
+                    {
+                        continue;
+                    }
+                    std::cout << "    " << ri->path() << '\n';
+                }
+            }
+            else
+                std::cout << path << " exists, but is not a regular file or directory\n";
+        }
+        else
+            std::cout << path << " does not exist\n";
     }
     catch (std::exception& e)
     {

ff69bfa21fc8cc35ef312f43725f1a19a187b8a0
OmarovAmir <omarov.amir.95@gmail.com>
2023-11-26 23:18:25 +0700

    Исправил ошибку при выполнении pop в end

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+#include <boost/filesystem.hpp>
 #include <iostream>
 #include <options.hpp>
-#include <boost/filesystem.hpp>
 namespace fs = boost::filesystem;
 
 int main(int argc, char** argv)
@@ -14,33 +14,38 @@ int main(int argc, char** argv)
             opts.print(std::cout);
             return 0;
         }
-
-        fs::path path = vm["include-paths"].as<paths>()[0];
         std::size_t depth = vm["depth"].as<std::size_t>();
-
-        if (fs::exists(path))
+        for (const auto& path_str : vm["include-paths"].as<paths>())
         {
-            if (fs::is_regular_file(path))
-                std::cout << path << " size is " << fs::file_size(path) << '\n';
-
-            else if (fs::is_directory(path))
+            fs::path path{path_str};
+            if (fs::exists(path))
             {
-                std::cout << path << " is a directory containing:\n";
+                if (fs::is_regular_file(path))
+                    std::cout << path << " size is " << fs::file_size(path) << '\n';
 
-                for (auto ri = fs::recursive_directory_iterator(path); ri != fs::recursive_directory_iterator(); ++ri)
+                else if (fs::is_directory(path))
                 {
-                    if(ri.depth() > static_cast<int>(depth))
+                    std::cout << path << " is a directory containing:\n";
+
+                    for (auto ri = fs::recursive_directory_iterator(path); ri != fs::end(ri); ++ri)
                     {
-                        ri.pop();
+                        if (ri.depth() > static_cast<int>(depth))
+                        {
+                            ri.pop();
+                            if (ri == fs::end(ri))
+                            {
+                                break;
+                            }
+                        }
+                        std::cout << "    " << ri->path() << '\n';
                     }
-                    std::cout << "    " << ri->path() << '\n';
                 }
+                else
+                    std::cout << path << " exists, but is not a regular file or directory\n";
             }
             else
-                std::cout << path << " exists, but is not a regular file or directory\n";
+                std::cout << path << " does not exist\n";
         }
-        else
-            std::cout << path << " does not exist\n";
     }
     catch (std::exception& e)
     {

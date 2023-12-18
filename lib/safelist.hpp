c59492be951564f961d7acdaaa60c1df93527e2b
omarov.amir <Amir.Omarov@infotecs.ru>
2023-12-18 15:33:04 +0700

    Попытка реализовать safelist

--- lib/safelist.hpp
+++ lib/safelist.hpp
@@ -1,15 +1,46 @@
 #pragma once
 #include <list>
 #include <mutex>
-#include <shared_mutex>
+#include <algorithm>
+#include <functional>
 
 template <typename T> class SafeList
 {
     std::list<T> _data;
-    std::shared_mutex _mutex;
+    std::mutex _mutex;
 
   public:
-    
+     std::list<T> readData(std::function<bool(T)> func)
+     {
+          std::unique_lock lock(_mutex);
+          std::list<T> result;
+
+          for(auto it = _data.begin(); it != _data.end();)
+          {
+               if(func(*it))
+               {
+                    result.push_back(std::move(*it));
+                    it = _data.erase(it);
+               }
+               else
+               {
+                    ++it;
+               }
+          }
+
+          return result;
+     }
+
+     void writeData(std::list<T>& data)
+     {
+          std::unique_lock lock(_mutex);
+          while(!data.empty())
+          {
+               _data.push_back(std::move(data.front()));
+               data.pop_front();
+          }
+          // запись данных назад и condition_variable notify_all();
+     }
 };
 
 // public getData for read or write data
